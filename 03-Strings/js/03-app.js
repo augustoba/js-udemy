c02fc5a2b7f2e01ab771579f12f8470e03c38b9d
augustoba <yo_augusto15@hotmail.com>
2023-09-10 00:30:05 -0300

    4 commit

--- 03-Strings/js/03-app.js
+++ 03-Strings/js/03-app.js
@@ -1,1 +1,4 @@
-const producto= "monitor 20 pulgadas";
+const producto= "monitor 20 pulgadas ";
+const precio= '30 USD';
+
+console.log(producto.concat(precio))
---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,15 @@
+//declaracion de funcion function declaration
+
+function sumar(){
+   console.log(2+2);
+}
+
+sumar();
+
+//function expression
+
+const sumar2= function (){
+    console.log(3+3);
+}
+
+sumar2()
